84de6cdf9f8821773b0bb153768404df558b5f48
Daniel Froes <danielfroestbf@gmail.com>
2022-05-04 19:33:18 -0300

    feat: adicionado skills

---
+++ skills.js
@@ -0,0 +1,6 @@
+function displaySkills(){
+    var vira = document.getElementById("vira");
+    vira.classList.toggle("virar");
+    var vira = document.getElementById("img-skills")
+    vira.classList.toggle("ativo")
+}
